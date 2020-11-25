2dbbd3016fdfbc8ce1618b4dee89182be821b351
Wh1teT1ger <burylov01@mail.ru>
2020-11-25 18:44:52 +0500

    add files

---
+++ include/Stack.hpp
@@ -0,0 +1,103 @@
+// Copyright 2020 Your Name <your_email>
+
+#ifndef TEMPLATE_STACK_HPP
+#define TEMPLATE_STACK_HPP
+
+#include <cstddef>
+template <typename T>
+class Stack {
+  struct Node {
+    T value;
+    Node* _prev;
+  };
+  size_t _size;
+  Node* _heap;
+
+ public:
+  Stack();
+  ~Stack();
+  Stack(const Stack&) = delete;
+  Stack& operator=(const Stack&) = delete;
+  Stack(Stack&& stack) noexcept;
+  Stack& operator=(Stack&& stack) noexcept;
+
+  void push(T&& value);
+  void push(const T& value);
+  void pop();
+  const T& head() const;
+  template <typename... Args>
+  void push_emplace(Args&&... value);
+  size_t size() const;
+};
+
+template <typename T>
+Stack<T>::Stack() : _size(0), _heap(nullptr) {}
+
+template <typename T>
+Stack<T>::~Stack() {
+  auto top = _heap;
+  while (top != nullptr) {
+    auto prev = top->_prev;
+    delete top;
+    top = prev;
+  }
+}
+
+template <typename T>
+Stack<T>::Stack(Stack<T>&& stack) noexcept {
+  _size = std::move(stack._size);
+  _heap = std::move(stack._heap);
+}
+
+template <typename T>
+Stack<T>& Stack<T>::operator=(Stack<T>&& stack) noexcept {
+  std::swap(_size, stack._size);
+  std::swap(_heap, stack._heap);
+  return *this;
+}
+
+template <typename T>
+void Stack<T>::push(T&& value) {
+  _heap = new Node{std::move(value), _heap};
+  _size++;
+}
+
+template <typename T>
+void Stack<T>::push(const T& value) {
+  _heap = new Node{value, _heap};
+  _size++;
+}
+
+template <typename T>
+void Stack<T>::pop() {
+  auto heap = _heap;
+  if (heap == nullptr) return;
+  auto new_heap = _heap->_prev;
+  if (new_heap == nullptr) {
+    _heap = nullptr;
+    _size = 0;
+  } else {
+    _heap = new_heap;
+    _size--;
+  }
+  delete heap;
+}
+
+template <typename T>
+const T& Stack<T>::head() const {
+  return _heap->value;
+}
+
+template <typename T>
+template <typename... Args>
+void Stack<T>::push_emplace(Args&&... value) {
+  _heap = new Node{T(std::forward<Args>(value)...), _heap};
+  _size++;
+}
+
+template <typename T>
+size_t Stack<T>::size() const {
+  return _size;
+}
+
+#endif  // TEMPLATE_STACK_HPP
--- tests/test.cpp
+++ tests/test.cpp
@@ -1,7 +1,56 @@
 // Copyright 2020 Your Name <your_email>
 
 #include <gtest/gtest.h>
+#include <Stack.hpp>
 
-TEST(Example, EmptyTest) {
-    EXPECT_TRUE(true);
+TEST(Stack, constructor){
+  Stack<int> stack;
+  EXPECT_EQ(stack.size(), 0);
+  stack.push(1);
+  EXPECT_EQ(stack.size(), 1);
+}
+
+TEST(Stack, push_rvalue){
+  Stack<int> stack;
+  stack.push(1);
+  EXPECT_EQ(stack.size(), 1);
+  EXPECT_EQ(stack.head(), 1);
+  stack.push(2);
+  EXPECT_EQ(stack.size(), 2);
+  EXPECT_EQ(stack.head(), 2);
+}
+
+TEST(Stack, push_lvalue){
+  int value = 2;
+  Stack<int> stack;
+  stack.push(value);
+  EXPECT_EQ(stack.size(), 1);
+  EXPECT_EQ(stack.head(), 2);
+  value = 3;
+  stack.push(value);
+  EXPECT_EQ(stack.size(), 2);
+  EXPECT_EQ(stack.head(), 3);
+}
+
+TEST(Stack, push_emplace){
+  Stack<std::string> stack;
+  stack.push_emplace(3, 't');
+  EXPECT_EQ(stack.size(), 1);
+  EXPECT_EQ(stack.head(), "ttt");
+  stack.push_emplace("hello");
+  EXPECT_EQ(stack.size(), 2);
+  EXPECT_EQ(stack.head(), "hello");
+}
+
+TEST(Stack, pop){
+  Stack<int> stack;
+  stack.push(1);
+  stack.push(2);
+  EXPECT_EQ(stack.size(), 2);
+  EXPECT_EQ(stack.head(), 2);
+  stack.pop();
+  EXPECT_EQ(stack.size(), 1);
+  EXPECT_EQ(stack.head(), 1);
+  stack.pop();
+  EXPECT_EQ(stack.size(), 0);
 }
